99e007466dba2a1d4accd82b8a100d2ff7d151c3
aruviyn <aruviyn@gmail.com>
2022-04-10 16:58:01 +0800

    classifiers setup

--- app.js
+++ app.js
@@ -5,6 +5,39 @@ const port = 3000
 const tf = require('@tensorflow/tfjs')
 // import * as tf from '@tensorflow/tfjs';
 
+const MOBILE_NET_INPUT_WIDTH = 224;
+const MOBILE_NET_INPUT_HEIGHT = 224;
+const STOP_DATA_GATHER = -1;
+
+let mobilenet = undefined;
+let gatherDataState = STOP_DATA_GATHER;
+let videoPlaying = false;
+let trainingDataInputs = [];
+let trainingDataOutputs = [];
+let examplesCount = [];
+let predict = false;
+
+
+/**
+ * Loads the MobileNet model and warms it up so ready for use.
+ **/
+ async function loadMobileNetFeatureModel() {
+  const URL = 
+    'https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v3_small_100_224/feature_vector/5/default/1';
+  
+  mobilenet = await tf.loadGraphModel(URL, {fromTFHub: true});
+  console.log('MobileNet v3 loaded successfully!');
+  
+  // Warm up the model by passing zeros through it once.
+  tf.tidy(function () {
+    let answer = mobilenet.predict(tf.zeros([1, MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH, 3]));
+    console.log(answer.shape);
+  });
+}
+
+// Call the function immediately to start loading.
+loadMobileNetFeatureModel();
+
 app.get('/', (req, res) => {
   
 
@@ -23,9 +56,9 @@ model.fit(xs, ys, {epochs: 10}).then(() => {
   // Use the model to do inference on a data point the model hasn't seen before:
   model.predict(tf.tensor2d([5], [1, 1])).print();
   // Open the browser devtools to see the output
-});
+}); 
 
-res.send('Hello 1!')
+res.send('Hello 09091209012!')
 
 })
 
